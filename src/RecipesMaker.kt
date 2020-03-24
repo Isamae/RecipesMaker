87bcd40e88c1acacec7017032886ac251636ba10
Isamae <diego.ismael.montesdeoca@gmail.com>
2020-03-24 17:29:55 -0500

    update funtion

--- src/RecipesMaker.kt
+++ src/RecipesMaker.kt
@@ -1,21 +1,40 @@
 fun main(args:Array<String>){
     var opcion:String? = "0"
     while(opcion !="3" ){
-        menu()
-        opcion= readLine()
+        opcion= menu()
         when(opcion){
             "3" -> println("Saliendo")
-            "2" -> println("Viendo Recetas")
-            "1" -> println("Haciendo Receta")
+            "2" -> viewRecipe()
+            "1" -> makeRecipe()
             else -> println("Opción Equivocada")
         }
     }
 }
-fun menu(){
+fun makeRecipe():String?{
+    println("""             
+            Hacer receta
+    Selecciona por categoría el ingrediente que buscas
+    1. Agua
+    2. Leche
+    3. Carne
+    4. Verduras
+    5. Frutas
+    6. Cereal
+    7. Huevos
+    8. Aceites""")
+    var opcion:String? = readLine()
+    return  opcion
+}
+fun viewRecipe(){
+    println("Viendo Recetas")
+}
+fun menu():String?{
     println("""             
         :: Bienvenido a Recipe Maker ::
           Selecciona la opción deseada
     1. Hacer una receta
     2. Ver mis recetas
     3. Salir""")
+    var opcion:String? = readLine()
+    return opcion
 }
